c0d0a263c08d4d3db1757a66f2c0a18359f5f380
jason chen <jason chen>
2016-10-19 22:39:26 -0700

    able to send bets to server

--- script.js
+++ script.js
@@ -41,26 +41,26 @@ function get_games(date_data,league_data) {
                 var game_landing = $('<div>').addClass('game_landing');
                 var bet_landing = $('<div>').addClass('bet_landing');
                 //bet data
-                $('<span>').text(response[i].spread[0],response[i].spread[2]).appendTo(bet).on('click', function(i){return function (){
-                    high_light(this,i,response[i].spread[0],'');
+                $('<span>').text(response[i].spread[0]+" "+response[i].spread[2]).appendTo(bet).on('click', function(i){return function (){
+                    high_light(this,i,'spread',0,response[i].spread[0],response[i].spread[2]);
                 }}(i));
-                $('<span>').text(response[i].spread[1],response[i].spread[3]).appendTo(bet).on('click',function(i){return function (){
-                    high_light(this,i,response[i].spread[1],'');
+                $('<span>').text(response[i].spread[1]+' '+response[i].spread[3]).appendTo(bet).on('click',function(i){return function (){
+                    high_light(this,i,'spread',1,response[i].spread[1],response[i].spread[3]);
                 }}(i));
                 $('<span>').text(response[i].money_line[0]).appendTo(bet).on('click', function(i){return function (){
-                    high_light(this,i,response[i].money_line[0],'');
+                    high_light(this,i,'money_line',0,response[i].money_line[0],response[i].money_line[0]);
                 }}(i));
                 $('<span>').text(response[i].money_line[1]).appendTo(bet).on('click', function(i){return function (){
-                    high_light(this,i,response[i].money_line[1],'');
+                    high_light(this,i,'money_line',1,response[i].money_line[1],response[i].money_line[1]);
                 }}(i));
-                $('<span>').text("OVER "+response[i].over_under).appendTo(bet).on('click', function(i){return function (){
-                    high_light(this,i,response[i].over_under,'over','');
+                $('<span>').text("UNDER "+response[i].over_under[0]).appendTo(bet).on('click', function(i){return function (){
+                    high_light(this,i,'money_line',0,response[i].over_under[0],response[i].over_under[1]);
                 }}(i));
-                $('<span>').text("UNDER "+response[i].over_under).appendTo(bet).on('click', function(i){return function (){
-                    high_light(this,i,response[i].over_under,'under','');
+                $('<span>').text("OVER "+response[i].over_under[0]).appendTo(bet).on('click', function(i){return function (){
+                    high_light(this,i,'money_line',1,response[i].over_under[0],response[i].over_under[2]);
                 }}(i));
                 $('<span>').text('CONFIRM').addClass('confirm').appendTo(bet).on('click', function (){
-                    high_light(this,'confirm');
+                    send_data(this);
                 });
                 //game data
                 $('<span>').text(response[i].away_long).addClass('teams_playing').appendTo(game);
@@ -78,12 +78,28 @@ function get_games(date_data,league_data) {
     });
 }
 
-function high_light(highlighter, confirm,game_id,type_bet,side,current_line){
-
+function high_light(highlighter, game_id1,type_of_bet1,side1,bet_line1,current_odds1){
+    high_light.bet_data={
+      game_id: game_id1,
+      type_of_bet:type_of_bet1,
+      side:side1,
+      bet_line:bet_line1,
+      current_odds:current_odds1
+    };
     $(highlighter).addClass('selected');
-    console.log('game id: '+game_id);
-    console.log('type bet: '+type_bet);
-    console.log('side: '+side);
-    console.log('current line: '+current_line);
-    console.log('confirm: '+ confirm);
+    console.log('bet data being saved: ',high_light.bet_data);
+}
+
+function send_data(highlighter) {
+    $(highlighter).addClass('selected');
+    this.bet_data = high_light.bet_data;
+    $.ajax({
+        dataType: 'json',
+        data: this.bet_data,
+        url: "dummy_data.php",
+        success: function (response) {
+            console.log('bet data being sent is: ',bet_data);
+            console.log("success?", response);
+        }
+    });
 }
