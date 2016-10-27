4a7f928b68dff5e5bf01e17f08e942c6f4959411
Kyle Marx <Kyle Marx>
2016-10-26 19:06:58 -0700

    started pages to query database

---
+++ add_bet_to_db.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kyle
+ * Date: 10/26/2016
+ * Time: 12:14 PM
+ */
+
+date_default_timezone_set('America/New_York');
+require_once('mysql_connect.php');
+
+$bet_query = '';
+
+?>
---
+++ get_game_data.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kyle
+ * Date: 10/26/2016
+ * Time: 5:06 PM
+ */
+
+date_default_timezone_set('America/Los_Angeles');
+
+require_once('mysql_connect.php');
+
+$query = "";
+
+?>
