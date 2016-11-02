fc398cecbe7c6268e26f5b8d3ac9e7ec0bdbc153
Kyle Marx <Kyle Marx>
2016-11-02 16:57:04 -0700

    we can now dynamically change the league the retrieve game data file sends

--- retrieve_game_data.php
+++ retrieve_game_data.php
@@ -3,40 +3,36 @@ require_once('mysql_connect.php');
 date_default_timezone_set('UTC');
 //$league = 'NFL';
 
-print_r($_POST);
+//print(json_encode($_POST));     //for testing purposes
 
 //collect league from index
-//$league = strtoupper($_POST['league']);  //may not need to upper
-////lookup the league in league table
-//$league_query = "SELECT ID FROM `leagues` WHERE league = '$league'";
-//$result = mysqli_query($conn, $league_query);
-//print_r($result);
-//if(mysqli_num_rows($result)){
-//    while($row = mysqli_fetch_assoc($result)) {
-//        $league_id = $row['ID'];
-//    }
-//}
-//
+$league = $_POST['league'];  //may not need to upper
+//lookup the league in league table
+$league_query = "SELECT ID FROM `leagues` WHERE league = '$league'";
+$result = mysqli_query($conn, $league_query);
+//print_r($result);             //for testing
+if(mysqli_num_rows($result)){
+    while($row = mysqli_fetch_assoc($result)) {
+        $league_id = $row['ID'];
+    }
+}
 
+//create empty array (of object)
+$data = [];
+//$league_id = $_POST[$league_id];
+//$league_id = 889;
+//$league_id = 487;
 
-//
-//
-////create empty array (of object)
-//$data = [];
-////$league_id = $_POST[$league_id];
-////$league_id = 889;
-////$league_id = 487;
-//
-////full data attempt
-//$game_query = "SELECT g.ID AS game_id, a.full_name AS full_name_a, a.abbr_name AS abbr_name_a, a.logo_src AS logo_src_a, h.full_name AS full_name_h, h.abbr_name AS abbr_name_h, h.logo_src AS logo_src_h, g.home_spread, g.spread_odds_a, g.spread_odds_h, g.moneyline_odds_a, g.moneyline_odds_h, g.overunder_points, g.overunder_odds_o, g.overunder_odds_u, g.game_time FROM `games` AS g JOIN `teams` AS h ON g.team_h_id = h.ID JOIN `teams` AS a ON g.team_a_id = a.ID WHERE g.league_id = $league_id ORDER BY g.game_time";
-//$result = mysqli_query($conn, $game_query);
-//if(mysqli_num_rows($result)){
-//    while($row = mysqli_fetch_assoc($result)) {
-//        $data[] = $row;
-//    }
-//}
-////json encode the data
-//$json_encoded_object = json_encode($data);
-////print the json encoded object
-//print($json_encoded_object);
-//?>
+//full data attempt
+$game_query = "SELECT g.ID AS game_id, a.full_name AS full_name_a, a.abbr_name AS abbr_name_a, a.logo_src AS logo_src_a, h.full_name AS full_name_h, h.abbr_name AS abbr_name_h, h.logo_src AS logo_src_h, g.home_spread, g.spread_odds_a, g.spread_odds_h, g.moneyline_odds_a, g.moneyline_odds_h, g.overunder_points, g.overunder_odds_o, g.overunder_odds_u, g.game_time FROM `games` AS g JOIN `teams` AS h ON g.team_h_id = h.ID JOIN `teams` AS a ON g.team_a_id = a.ID WHERE g.league_id = $league_id ORDER BY g.game_time";
+$result = mysqli_query($conn, $game_query);
+if(mysqli_num_rows($result)){
+    while($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+}
+//json encode the data
+$json_encoded_object = json_encode($data);
+//print the json encoded object
+print($json_encoded_object);
+?>
