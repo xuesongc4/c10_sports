b976125112752c8d800314090753a26e8fce324a
Kyle Marx <Kyle Marx>
2016-11-04 10:39:46 -0700

    retrieve bet history is now sending queried info

--- api/retrieve_bet_history.php
+++ api/retrieve_bet_history.php
@@ -5,8 +5,15 @@ date_default_timezone_set('UTC');
 
 $user_id = $_SESSION['ID'];
 //$user_id = '1';           //for testing only
-$bet_history_query = "SELECT amount, settled, bet_type_id, game_id, side, line, odds FROM `bets` WHERE user_id = '$user_id'";
-
+//$bet_history_query = "SELECT amount, settled, bet_type_id, game_id, side, line, odds FROM `bets` WHERE user_id = '$user_id'";
+$bet_history_query = "SELECT b.amount, bs.bet_status, a.full_name AS away_team, a.logo_src AS away_logo, h.full_name AS home_team, h.logo_src AS home_logo, g.game_time, bt.bet_name, b.side, b.odds
+FROM `bets` AS b
+JOIN `games` AS g ON g.ID = b.game_id
+JOIN `bet_status` AS bs ON bs.ID = b.settled
+JOIN `teams` AS h ON g.team_h_id = h.ID
+JOIN `teams` AS a ON g.team_a_id = a.ID
+JOIN `bet_types`AS bt ON bt.ID = b.bet_type_id
+WHERE b.user_id = '$user_id'";
 //full data attempt
 $result = mysqli_query($connection, $bet_history_query);
 if(mysqli_num_rows($result)){
@@ -24,12 +31,12 @@ $output = [
 
 ];
 
-$encoded_output = json_encode($output);
-print($encoded_output);
+//$encoded_output = json_encode($output);
+//print($encoded_output);
 
 
 //json encode the data
-//$json_encoded_object = json_encode($data);
+$json_encoded_object = json_encode($data);
 //print the json encoded object
-//print($json_encoded_object);
+print($json_encoded_object);
 ?>
