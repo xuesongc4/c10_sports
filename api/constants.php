73b8118945915bd6c2a60fc685569dee558d823c
unknown <dan.dh.lee@gmail.com>
2016-11-07 19:46:30 -0800

    Fix image names

---
+++ api/constants.php
@@ -0,0 +1,9 @@
+<?php 
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'betting');
+define('API_CLIENT_ID', 'DC914164');
+define('API_PASS', 'lfz9080!');
+
+ ?>
