5acd565cfed223606e4e5b8370b26b252dc7cb52
Kyle Marx <Kyle Marx>
2016-11-09 17:13:23 -0800

    retrieve game data now sends bet information for specific user

--- api/retrieve_game_data.php
+++ api/retrieve_game_data.php
@@ -1,25 +1,28 @@
 <?php
+session_start();
 require_once('mysql_connect.php');
 date_default_timezone_set('UTC');
 
-//hardcorded constants for testing
-$user_id = '2';             //do i need session variable and user now that this is not generic but specific to user because
-$time_frame = 'current';
-////day one
-$start_day = '2016-11-04 00:00:00';
-$end_day = '2016-11-05 00:00:00';
-//second test day
-//$start_day = '2016-11-09 00:00:00';
-//$end_day = '2016-11-10 00:00:00';
-//league
-$league = 'NFL';
+////hardcorded constants for testing
+//$user_id = '2';             //do i need session variable and user now that this is not generic but specific to user because
+//$time_frame = 'current';
+//////day one
+//$start_day = '2016-11-04 00:00:00';
+//$end_day = '2016-11-05 00:00:00';
+////second test day
+////$start_day = '2016-11-09 00:00:00';
+////$end_day = '2016-11-10 00:00:00';
+////league
+//$league = 'NFL';
 
 //print(json_encode($_POST));     //for testing purposes
-//$time_frame = $_POST['game_block'];
-//$start_day = $_POST['start_end']['startDay'];
-//$end_day = $_POST['start_end']['endDay'];
-////collect league from index
-//$league = $_POST['league'];
+$time_frame = $_POST['game_block'];
+$start_day = $_POST['start_end']['startDay'];
+$end_day = $_POST['start_end']['endDay'];
+//collect league from index
+$league = $_POST['league'];
+//gather user id
+$user_id = $_SESSION['ID'];
 //lookup the league in league table
 $league_query = "SELECT ID FROM `leagues` WHERE league = '$league'";
 $result = mysqli_query($connection, $league_query);
@@ -57,11 +60,12 @@ $game_query = $game_query_select_clause . $game_query_from_clause . $game_query_
 //query the db
 $game_result = mysqli_query($connection, $game_query);
 
-$bets_placed = ['bets_placed'=>['spread'=>'false','moneyline'=>'false','over_under'=>'false']];
+$bets_placed = ['bets_placed'=>['spread'=>'0','moneyline'=>'0','over/under'=>'0']];
 
 if(mysqli_num_rows($result)){
     while($row = mysqli_fetch_assoc($game_result)) {
         $data[] = array_merge($row, $bets_placed);
+//        $data[] = $row;
     }
 }
 
@@ -86,55 +90,80 @@ $bet_query_group_clause = "GROUP by g.ID, b.bet_type_id ";
 $bet_query_order_clause = "ORDER BY g.ID, b.bet_type_id";
 ////concatenate query clauses together
 $bet_query = $bet_query_select_clause . $bet_query_from_clause . $bet_query_join_clause . $bet_query_where_clause . $bet_query_group_clause . $bet_query_order_clause;
-print($bet_query);
+//print($bet_query);
 $bet_results = mysqli_query($connection, $bet_query);
 
-//$games = [];
+$games_bet = [];
 if(mysqli_num_rows($bet_results)){
     //if there are any results see if there are any of the appropriate keys in the result
 //    print('we got a result');
-    $games = [];           //maybe dan's
+//    $games = [];           //maybe dan's
     $old_game_id = null;             //dan's code
     $this_game = null;               //dan's code
     while($row = mysqli_fetch_assoc($bet_results)){              //dan's code
 //        $games[] = $row;
         if($old_game_id != $row['game_id']){           //dan's code
-            $games[$row['game_id']] = $this_game;          //dan's code
+            $games_bet[$row['game_id']] = $this_game;          //dan's code
             $this_game = [             //dan's code
-                'game_id'=> $row['game_id'],
-                'spread'=>null,     //dan's code
-                'moneyline'=>null,     //dan's code
-                'over/under'=>null     //dan's code
+//                'game_id'=> $row['game_id'],
+                'spread'=>0,     //dan's code
+                'moneyline'=>0,     //dan's code
+                'over/under'=>0     //dan's code
             ];
             $old_game_id = $row['game_id'];                //dan's code
         }
         $this_game[$row['bet_name']] = $row['bet_qty'];     //dan's code
-        $this_game['game_id'] = $row['game_id'];              //my line
-        $games[$row['game_id']] = $this_game;
+//        $this_game['game_id'] = $row['game_id'];              //my line
+        $games_bet[$row['game_id']] = $this_game;
     }
 //    array_shift($games);     //dan's code
-    print('<pre>');
-    print('<br>');
-    print_r($games);
-    print('</pre>');
-}else{
-    $games = [];
-    print('<br>no results');        //for testing only
+//    print('<pre>');
+//    print('<br>');
+//    print_r($games_bet);
+//    print('</pre>');
 }
 
-//////for testing
-print('<pre>');
-print_r($data);
-print('</pre>');
+//get help from dan looking through this
+//**************************************************************************
+for($i = 0; $i < count($data); $i++) {
+    $index = $data[$i]['game_id'];
+    if(array_key_exists($index, $games_bet)){
+        $data[$i]['bets_placed'] = $games_bet[$index];
+    }
+}
 
+
+//$curr_index = 0;
+//$key_arr = [];
+//foreach ($games_bet as $gb){
+//    for($index = $curr_index; $index < count($data); $index++){
+//        print(key($gb));
+////        $key_arr[] = key($gb);
+////        if($data[$index]['game_id'] === key($gb)){
+//////            $key_arr[] = key($games_bet);
+////            $curr_index = $index;
+////            $data[$index]['bets_placed'] = $gb;
+////            break;
+////        }
+//    }
+//}
+
+
+//////for testing
 //print('<pre>');
-////print_r($data);
+//print_r($data);
+//print('</pre>');
+//
+//print('<pre>');
+////print_r($key_arr);
+////print('<br>');
+//print_r($data);
 //print('<br>');
 //print_r($games);
 //print('</pre>');
 
-////json encode the data
-//$json_encoded_object = json_encode($data);
-////print the json encoded object
-//print($json_encoded_object);
+//json encode the data
+$json_encoded_object = json_encode($data);
+//print the json encoded object
+print($json_encoded_object);
 ?>
