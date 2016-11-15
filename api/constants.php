c267f129da5089847f39d5aa9a2b4817c7487d28
kylemarx86 <kylemarx86@csu.fullerton.edu>
2016-11-15 15:28:54 -0800

    Delete constants.php
    shouldn't exist

--- api/constants.php
+++
@@ -1,9 +0,0 @@
-<?php 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'dev_sports_betting');
-define('API_CLIENT_ID', 'DC914164');
-define('API_PASS', 'lfz9080!');
-
- ?>
