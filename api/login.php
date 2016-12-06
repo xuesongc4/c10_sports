98b33945f21dd8eb7dd64610339ca6107e1d1ac5
unknown <dan.dh.lee@gmail.com>
2016-12-06 01:48:50 -0800

    add signup query to login page

--- api/login.php
+++ api/login.php
@@ -21,26 +21,42 @@ if (isset($_POST['login'])) {
 		$row = mysqli_fetch_assoc($find_user_result);
 		$_SESSION['ID'] = $row['ID'];
 		$_SESSION['username'] = $username;
+	} else {
+		$_SESSION['message'] = 'Incorrect username or password';
 	}
 	
 	header('Location: ../');
 }
 
 if (isset($_POST['guest'])){
-    $username = mysqli_real_escape_string($connection, 'guest');
-    $encrypted_password = sha1('guest');
+  $username = mysqli_real_escape_string($connection, 'guest');
+  $encrypted_password = sha1('guest');
 
-    $find_user_query = "SELECT ID FROM users WHERE username ='{$username}' AND password='{$encrypted_password}'";
-    $find_user_result = mysqli_query($connection, $find_user_query);
-    if (!$find_user_query) {
-        die("QUERY FAIlED " . mysqli_error($connection));
-    }
-    if(mysqli_num_rows($find_user_result)>0){
-        $row = mysqli_fetch_assoc($find_user_result);
-        $_SESSION['ID'] = $row['ID'];
-        $_SESSION['username'] = $username;
-    }
+  $find_user_query = "SELECT ID FROM users WHERE username ='{$username}' AND password='{$encrypted_password}'";
+  $find_user_result = mysqli_query($connection, $find_user_query);
+  if (!$find_user_result) {
+      die("QUERY FAIlED " . mysqli_error($connection));
+  }
+  if(mysqli_num_rows($find_user_result)>0){
+      $row = mysqli_fetch_assoc($find_user_result);
+      $_SESSION['ID'] = $row['ID'];
+      $_SESSION['username'] = $username;
+  }
 
-    header('Location: ../');
+  header('Location: ../');
+}
+
+if (isset($_POST['signup'])) {
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$password = sha1($_POST['password']);
+	$create_user_query = "INSERT INTO users (username, email, password) VALUES ($username, $email, $password)";
+	$create_user_result = mysqli_query($connection, $create_user_query);
+	if (!$create_user_result) {
+    die("QUERY FAIlED " . mysqli_error($connection));
+  }
+  $_SESSION['message'] = "User created!";
+
+  header('Location ../');
 }
  ?>
